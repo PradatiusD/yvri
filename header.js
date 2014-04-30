db545bb34a764562516117ea4668591f300873cb
PradatiusD <danielprada2012@gmail.com>
2014-04-30 12:02:07 -0400

    IE 7 & 8 fixes to navigation, slider, widgets

--- header.js
+++ header.js
@@ -1,6 +1,15 @@
-(function($){
+(function addMobileNavigation($){
 	var $menuButton = $('<li id="menuButton" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home"><i class="fa fa-bars"></i><a href="#">Menu</a></li>');
 	var $navMenu    = $('#menu-primary-menu');
+
+	// Search for About us and hide it on mobile
+	$navMenu.find('li').each(function(){
+		var $this = $(this);
+
+		if ($this.text().indexOf('About Us')>-1) {
+			$this.addClass('about-us-nav');
+		}
+	});
 
 	$navMenu.prepend($menuButton);
 	$menuButton.click(function(e){
